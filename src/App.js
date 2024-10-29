f04d9bcd106ff25476609d4302c88bbb4480aed3
AdamHaniff <AdamHaniffDeveloper@gmail.com>
2024-10-28 23:56:24 -0400

    Created Balance component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,21 @@
 export default function App() {
-  return <div>Hi, Adam Haniff</div>;
+  return (
+    <div className="app">
+      <Balance />
+    </div>
+  );
 }
+
+function Balance() {
+  return (
+    <div className="balance">
+      <div className="balance__info">
+        <span className="balance__label">My balance</span>
+        <span className="balance__number">$921.48</span>
+      </div>
+      <img className="balance__logo" src="images/logo.svg" alt="Logo" />
+    </div>
+  );
+}
+
+function Spending() {}

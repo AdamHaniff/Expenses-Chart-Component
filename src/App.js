a36959e2b34d4c7ad7d02566acee4e6d38aa28c3
AdamHaniff <AdamHaniffDeveloper@gmail.com>
2024-10-29 23:58:29 -0400

    Created ExpenseBarDay component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
+const expenses = [
+  {
+    day: "mon",
+    amount: 17.45,
+  },
+  {
+    day: "tue",
+    amount: 34.91,
+  },
+  {
+    day: "wed",
+    amount: 52.36,
+  },
+  {
+    day: "thu",
+    amount: 31.07,
+  },
+  {
+    day: "fri",
+    amount: 23.39,
+  },
+  {
+    day: "sat",
+    amount: 43.28,
+  },
+  {
+    day: "sun",
+    amount: 25.48,
+  },
+];
+
 export default function App() {
   return (
     <div className="app">
       <Balance />
+      <Spending />
     </div>
   );
 }
@@ -18,4 +50,25 @@ function Balance() {
   );
 }
 
-function Spending() {}
+function Spending() {
+  return (
+    <div className="spending">
+      <h2 className="spending__label">Spending - Last 7 days</h2>
+      <div className="spending__chart">
+        {expenses.map((expense) => (
+          <ExpenseBarDay key={expense.day} expense={expense} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function ExpenseBarDay({ expense }) {
+  return (
+    <div className="spending__expense-bar-day">
+      <span className="spending__expense">{expense.amount}</span>
+      <div className="spending__bar"></div>
+      <span className="spending__day">{expense.day}</span>
+    </div>
+  );
+}
